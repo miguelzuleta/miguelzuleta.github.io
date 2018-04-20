5818d919196cb0a36785e347afaeba42fcecf48d
Miguel Zuleta <mzuleta@apple.com>
2018-04-19 23:56:54 -0700

    Load message when browser is too old

--- components/js/main.js
+++ components/js/main.js
@@ -4,14 +4,33 @@ import RenderExp from './render-sections/RenderExp.js'
 import RenderContact from './render-sections/RenderContact.js'
 import rotatingHero from './rotatingHero.js'
 
+import render from './render'
+
+let body = document.querySelector('body')
+let isIE = !!document.documentMode || (!isIE && !!window.StyleMedia)
+
+if (!self.fetch) {
+	body.classList.add('old-browser')
+
+	let oldBrowserMsg = `
+		<div class="msg">
+			<p>Hello! This is Miguel.</p>
+			<p>This page didn't load because your browser is too old. :(</p>
+			<p>Have you tried <a href="https://www.google.com/chrome/">Google Chrome</a>?</p>
+		</div>
+	`
+
+	body.innerHTML = oldBrowserMsg
+
+	throw new Error("Old Browser!")
+}
+
 let data = fetch('./data.json')
 			.then(response => response.json())
 			.then(body => body)
 
 let dataReceived = Promise.resolve(data)
 
-let body = document.querySelector('body')
-let isIE = !!document.documentMode || (!isIE && !!window.StyleMedia)
 
 dataReceived.then(dataObj => {
 	let { hero, skills, exp, contact } = dataObj
