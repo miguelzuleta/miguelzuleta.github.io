694282186c419ba092b4a45f4ab121e605f9761a
mzuleta <zzuleta@hotmail.com>
2018-04-23 19:16:19 -0700

    Make deploy script push to test site

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,16 @@
-echo "Enter merge deploy commit message"
+echo "Enter test deploy commit message"
 read commitMessage
 
 function deleteInit() {
-	if [ -e init.sh ]
-	then
-		rm -rf init.sh
-	fi
+    if [ -e init.sh ]
+    then
+        rm -rf init.sh
+    fi
 }
 deleteInit
 
-function commitToBranch() {
-	git add .
-	git commit -m "$commitMessage"
-	git push
-}
-
 gulp --prod --deploy
-commitToBranch
+
 git checkout master
 
 deleteInit
@@ -24,6 +18,9 @@ deleteInit
 git checkout dev site
 mv site/* .
 rm -rf site
-commitToBranch
+
+git add .
+git commit -m "$commitMessage"
+git push test master
 
 git checkout dev
