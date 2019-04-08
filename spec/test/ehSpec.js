8949d4c5c0e86de874dad36c3703192a6f56987f
Miguel Zuleta <mzuleta@apple.com>
2019-04-08 11:33:01 -0700

    get async to work

--- spec/test/ehSpec.js
+++ spec/test/ehSpec.js
@@ -1,6 +1,20 @@
+import puppeteer from 'puppeteer'
 import MZsite from '../../components/js/MZsite.js'
 
 describe("MZsite runs", function() {
+	let browser;
+	let page;
+
+	beforeAll(async function() {
+		browser = await puppeteer.launch({ headless: true });
+		page = await browser.newPage();
+		await page.goto('https://miguelzuleta.com');
+		await page.waitForSelector('body.data-loaded', { visible: true })
+		const pageHTML = await page.evaluate(() => {
+			return document.querySelector('body').innerHTML;
+		});
+		console.log(pageHTML);
+	});
 
 	it("init() exists", function() {
 		expect(MZsite().init()).not.toBe(null);
