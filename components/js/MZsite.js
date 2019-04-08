d597e60fdd91f2871c53f11c4c9c3c103a9eae9d
Miguel Zuleta <mzuleta@apple.com>
2019-04-08 15:17:51 -0700

    test that all sections render

--- components/js/MZsite.js
+++ components/js/MZsite.js
@@ -1,24 +1,24 @@
-// import { BODY } from './helpers/constants.js'
+import { BODY } from './helpers/constants.js'
 
-// import RenderBg from './render-sections/RenderBg.js'
-// import RenderHero from './render-sections/RenderHero.js'
-// import RenderSkills from './render-sections/RenderSkills.js'
-// import RenderExp from './render-sections/RenderExp.js'
-// import RenderCodepens from './render-sections/RenderCodepens.js'
-// import RenderContact from './render-sections/RenderContact.js'
-// import RenderOldBrowserMsg from './render-sections/RenderOldBrowserMsg.js'
+import RenderBg from './render-sections/RenderBg.js'
+import RenderHero from './render-sections/RenderHero.js'
+import RenderSkills from './render-sections/RenderSkills.js'
+import RenderExp from './render-sections/RenderExp.js'
+import RenderCodepens from './render-sections/RenderCodepens.js'
+import RenderContact from './render-sections/RenderContact.js'
+import RenderOldBrowserMsg from './render-sections/RenderOldBrowserMsg.js'
 
-// import rotatingBG from './helpers/rotatingBG.js'
+import rotatingBG from './helpers/rotatingBG.js'
 
 let MZsite = () => {
 	let init = () => {
-		// if (!self.fetch) {
-		// 	RenderOldBrowserMsg()
-		// } else {
-		// 	_renderSections()
-		// }
+		if (!self.fetch) {
+			RenderOldBrowserMsg()
+		} else {
+			_renderSections()
+		}
 
-		return 9
+		// return 9
 	}
 
 	let _fetchData = jsonFile => {
--- spec/test/ehSpec.js
+++ spec/test/ehSpec.js
@@ -1,31 +1,77 @@
+// var jsdom = require("jsdom");
+
+// window = jsdom.jsdom('<html><head></head><body><div id="rondavu_container"></div></body></html>').createWindow();
+
+// if(Object.keys(window).length === 0) {
+//     // this hapens if contextify, one of jsdom's dependencies doesn't install correctly
+//     // (it installs different code depending on the OS, so it cannot get checked in.);
+//     throw "jsdom failed to create a usable environment, try uninstalling and reinstalling it";
+// }
+
+// global.window = window;
+
+// global.document = window.document;
+
 import puppeteer from 'puppeteer'
-import MZsite from '../../components/js/MZsite.js'
+// import MZsite from '../../site/js.js'
+// import MZsite from '../../components/js/MZsite.js'
+// import fetchData from '../../components/js/MZsite.js'
+// import fetch from 'node-fetch'
+// import promisedData from '../../site/data.json'
 
 describe("MZsite runs", function() {
 	let browser;
 	let page;
+	let waitOptions = {
+		visible: true,
+		timeout: 500
+	}
+
+	let renderSections = {
+		page: {
+			msg: "renders page",
+			selector: "body.data-loaded"
+		},
+		bg: {
+			msg: "renders bg wrapper section",
+			selector: ".bg-wrap"
+		},
+		hero: {
+			msg: "renders hero section",
+			selector: ".hero-msg-wrap"
+		},
+		skills: {
+			msg: "renders skills section",
+			selector: ".skills.info"
+		},
+		exp: {
+			msg: "renders expirience section",
+			selector: ".exp.info"
+		},
+		codepen: {
+			msg: "renders codepen section",
+			selector: ".codepens.info"
+		},
+		contact: {
+			msg: "renders contact section",
+			selector: ".contact.info"
+		}
+	}
 
 	beforeAll(async () => {
 		browser = await puppeteer.launch({ headless: true });
 		page = await browser.newPage();
-		await page.goto('https://miguelzuleta.com');
+		await page.goto('http://localhost:8080/');
 	});
 
-	it("page renders", async () => {
-		let pageLoads = await page.waitForSelector('body.data-loaded', {
-			visible: true,
-			timeout: 500
-		 })
+	for (let section in renderSections) {
+		let { msg, selector } = renderSections[section];
 
-		expect(pageLoads !== null).toBe(true);
-	})
+		it(msg, async () => {
+			let sectionLoad = await page.waitForSelector(selector, waitOptions);
 
-	it("init() exists", () => {
-		expect(MZsite().init()).not.toBe(null);
-	});
-
-	it("init() returns number", () => {
-		expect(MZsite().init()).toBe(9);
-	});
+			expect(sectionLoad !== null).toBe(true);
+		})
+	}
 
 });
