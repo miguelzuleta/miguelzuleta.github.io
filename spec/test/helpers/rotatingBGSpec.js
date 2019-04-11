6de3c8d43a3a92cfa03c6287873160600742eebf
Miguel Zuleta <mzuleta@apple.com>
2019-04-11 12:17:21 -0700

    revert styles

--- spec/test/helpers/rotatingBGSpec.js
+++ spec/test/helpers/rotatingBGSpec.js
@@ -45,18 +45,13 @@ describe('Dimensions of rotating hero background', () => {
 			await page.setViewport({ width: width, height: height });
 
 			let styles = await page.$eval('.bg-square', el => {
-
-				let computedStyle = parseFloat(
-					JSON.parse(
-						JSON.stringify(getComputedStyle(el)
-					)
-				));
+				let getStyles = JSON.parse(JSON.stringify(getComputedStyle(el)));
 
 				return {
-					width: computedStyle.width,
-					height: computedStyle.height,
-					top: computedStyle.top,
-					left: computedStyle.left
+					width: parseFloat(getStyles.width),
+					height: parseFloat(getStyles.height),
+					top: parseFloat(getStyles.top),
+					left: parseFloat(getStyles.left)
 				};
 			});
 
